9f711c89ad8e7ed879cdad281add58efb44289b7
salma <138569162+salmaa844@users.noreply.github.com>
2024-08-20 23:59:03 +0300

    session2

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace school.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
